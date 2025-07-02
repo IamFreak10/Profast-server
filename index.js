fb0f3a08a153ca9fa3ca78fce6d5d388da8c8d27
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2025-07-02 20:46:54 +0600

    Added usersCollection api via Post

--- index.js
+++ index.js
@@ -30,10 +30,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // DB and Collection
     const db = client.db('Profast');
     const ParcelCollection = db.collection('parcels');
+    const paymentsCollection = db.collection('payments');
+    const usersCollection = db.collection('users');
+
+    //User RElated Apis:
+    app.post('/users', async (req, res) => {
+      const email = req.body.email;
+      const user = req.body;
+      const userExists = await usersCollection.findOne({ email: email });
+      if (userExists) {
+        return res
+          .status(200)
+          .send({ message: 'User Already Exists', inserted: false });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Create a new parcel
     app.post('/parcels', async (req, res) => {
@@ -75,10 +91,11 @@ async function run() {
 
     // Stripe
     app.post('/create-payment-intent', async (req, res) => {
+      const amountInCents = req.body.amountInCents;
       try {
         const paymentIntent = await stripe.paymentIntents.create({
-          amount: 1000, // amount in cents
-          currency: 'usd',
+          amount: amountInCents, // amount in cents
+          currency: 'bdt',
           payment_method_types: ['card'],
         });
 
@@ -86,6 +103,62 @@ async function run() {
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
+    });
+
+    // POST: Record payment and update parcel status
+    app.post('/payments', async (req, res) => {
+      const { id, email, amount, paymentMethod, transactionId } = req.body;
+
+      try {
+        const updateResult = await ParcelCollection.updateOne(
+          { _id: new ObjectId(id) },
+          {
+            $set: { payment_status: 'paid' },
+          }
+        );
+
+        if (updateResult.matchedCount === 0) {
+          return res.status(404).send({ message: 'Parcel not found' });
+        }
+
+        const paymentDoc = {
+          parcel_id: id,
+          email,
+          amount,
+          paymentMethod,
+          transactionId,
+          paid_at_string: new Date().toISOString(),
+          paid_at: new Date(),
+        };
+
+        const paymentResult = await paymentsCollection.insertOne(paymentDoc);
+
+        // Send response back to client!
+        res.status(201).send({
+          message: 'Payment recorded and parcel marked as paid',
+          insertedId: paymentResult.insertedId,
+        });
+      } catch (error) {
+        console.error('Payment processing failed:', error);
+        res.status(500).send({ message: 'Failed to record payment' });
+      }
+    });
+
+    // GET:Get Payments
+    app.get('/payments', async (req, res) => {
+      try {
+        const userEmail = req.query.email;
+        const query = userEmail ? { email: userEmail } : {};
+        const options = {
+          sort: { paid_at: -1 },
+        };
+        const result = await paymentsCollection.find(query, options).toArray();
+        if (result.length > 0) {
+          res.send(result);
+        } else {
+          res.send([]);
+        }
+      } catch (error) {}
     });
 
     // Send a ping to confirm a successful connection
